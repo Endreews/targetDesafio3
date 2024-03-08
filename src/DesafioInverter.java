19186ed567cc0203fa3a58f7a3286e2b38bd7656
Endreews <150464197+Endreews@users.noreply.github.com>
2024-03-08 14:46:20 -0300

    Update DesafioInverter.java

--- src/DesafioInverter.java
+++ src/DesafioInverter.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class DesafioInverter {
+import java.util.Scanner;
+
+public class Desafio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("String: ");
+        String entrada = scanner.nextLine();
+        scanner.close();
+
+        // Chamada da função para inverter a string
+        String resultado = Inverter(entrada);
+
+        // Exibição do resultado
+        System.out.println("String original: " + entrada);
+        System.out.println("String invertida: " + resultado);
+    }
+
+    // Função para inverter a string
+    private static String Inverter(String s) {
+        int tamanho = s.length();
+        StringBuilder stringInvertida = new StringBuilder();
+        for (int i = tamanho - 1; i >= 0; i--) {
+            stringInvertida.append(s.charAt(i));
+        }
+        return stringInvertida.toString();
+    }
 }
+
